1f71917ae6f944e9025fad2196ae9431e2d94689
StolzFe <156649254+StolzFe@users.noreply.github.com>
2024-03-12 23:40:35 +0100

    Update Tanks.java

--- Tanks.java
+++ Tanks.java
@@ -3,10 +3,11 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Tanks extends JFrame implements KeyListener {
-    private String imagePath = "C:\\Schule\\ber.jpg"; // Aktualisierter Pfad zum Bild
+    private String imagePath = "C:\\Schule\\ber.jpg"; // Pfad zum Bild
     private ImageIcon imageIcon;
-    private int imageY = 0; // Startposition der Bilder
-    private int frameWidth = 1000; // Breite des Fensters
+    private int imageY = 0; // Startposition der Bilder in Y-Richtung
+    private int startX; // Startposition der Bilder in X-Richtung
+    private int frameWidth = 2000; // Breite des Fensters
     private int frameHeight = 1000; // Höhe des Fensters
     private boolean moving = false; // Zustand der Bewegung
 
@@ -19,15 +20,21 @@ public class Tanks extends JFrame implements KeyListener {
         // Bild laden
         imageIcon = new ImageIcon(imagePath);
 
+        // Berechne die Start-X-Position, um die Bilder zentral zu positionieren
+        int totalWidth = 3 * imageIcon.getIconWidth() + 2 * 10; // Gesamtbreite aller Bilder plus Abstand
+        startX = (frameWidth - totalWidth) / 2; // Zentriere horizontal
+
         // JPanel, das die Bilder zeichnet
         JPanel panel = new JPanel() {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
-                // Zeichne das erste Bild an der aktuellen Y-Position
-                g.drawImage(imageIcon.getImage(), 50, imageY, this);
-                // Zeichne die Kopie des Bildes direkt neben dem Original
-                g.drawImage(imageIcon.getImage(), 50 + imageIcon.getIconWidth() + 10, imageY, this); // 10 Pixel Abstand
+                // Zeichne das erste Bild an der berechneten X- und Y-Position
+                g.drawImage(imageIcon.getImage(), startX, imageY, this);
+                // Zeichne die Kopie des Bildes mit 10 Pixel Abstand vom ersten
+                g.drawImage(imageIcon.getImage(), startX + imageIcon.getIconWidth() + 10, imageY, this);
+                // Zeichne das dritte Bild mit 10 Pixel Abstand von der zweiten Kopie
+                g.drawImage(imageIcon.getImage(), startX + 2 * (imageIcon.getIconWidth() + 10), imageY, this);
             }
         };
 
@@ -44,12 +51,12 @@ public class Tanks extends JFrame implements KeyListener {
     }
 
     private void moveImages() {
-        new Timer(20, new ActionListener() {
+        new Timer(10, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 if (moving) {
-                    imageY += 5;
-                    if (imageY > frameHeight+1000 - imageIcon.getIconHeight()) {
+                    imageY += 10;
+                    if (imageY > frameHeight - imageIcon.getIconHeight()) {
                         imageY = 0; // Setze die Bilder zurück auf die ursprüngliche Position
                         moving = false; // Stoppe die Bewegung
                         ((Timer)e.getSource()).stop(); // Stoppe den Timer
